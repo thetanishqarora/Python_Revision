95c82c7091a82200b4f4327e09d29bc46ad41160
thetanishqarora <thetanishqarora@gmail.com>
2024-10-01 13:42:24 +0530

    1 Oct

--- main.py
+++ main.py
@@ -2,3 +2,10 @@
 
 print ("Hello World, Here we go again!")
 print ("This is for checking GitHub repos working fine!")
+
+# data types
+type(14)
+type(15.3)
+
+print(type(23))
+print("My name is {} and I am {} years old".format("Tanishq",22))
