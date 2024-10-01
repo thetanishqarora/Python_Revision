a8c3e4680bb6d0ba8677861ff4cda4b6358b50c2
thetanishqarora <thetanishqarora@gmail.com>
2024-10-01 13:49:16 +0530

    1 Oct b

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Revising python and making it STRONG
+import re
 
 print ("Hello World, Here we go again!")
 print ("This is for checking GitHub repos working fine!")
@@ -9,3 +10,12 @@ type(15.3)
 
 print(type(23))
 print("My name is {} and I am {} years old".format("Tanishq",22))
+
+#Reg Ex
+string = "This is to check the usage of Regular Expression in Python."
+pattern = r"age"
+result = re.search(pattern,string)
+if result:
+    print("Match Found")
+else:
+    print("No Match")
